5073b667fa240f9876a4af9432189628a3f2044f
hyeseong <hyeseong7848@gmail.com>
2023-07-25 22:49:50 +0900

    통계하기 화면 추가

--- Emotion/EmotionEnum.swift
+++ Emotion/EmotionEnum.swift
@@ -38,7 +38,42 @@ extension EmotionBgColor {
             case .dead:
                 return .systemCyan
             }
-    
+            
+        }
+    }
+}
+
+enum EmotionTitle: String, CaseIterable {
+    case happy = "완전행복지수"
+    case smile = "적당미소지수"
+    case basic = "그냥그냥지수"
+    case sad = "좀속상한지수"
+    case dead = "많이슬픈지수"
+}
+
+enum EmotionLabelColor: CaseIterable {
+    case happy
+    case smile
+    case basic
+    case sad
+    case dead
+}
+
+extension EmotionLabelColor {
+    var value: UIColor {
+        get {
+            switch self {
+            case .happy:
+                return .white
+            case .smile:
+                return .blue
+            case .basic:
+                return .link
+            case .sad:
+                return .red
+            case .dead:
+                return .black
+            }
         }
     }
 }
--- StatisticsViewController.swift
+++ StatisticsViewController.swift
@@ -9,9 +9,38 @@ import UIKit
 
 class StatisticsViewController: UIViewController {
 
+    @IBOutlet var emotionScoreLabel: [UILabel]!
+    @IBOutlet var emotionTitleLabel: [UILabel]!
+    @IBOutlet var containerViewCollection: [UIView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.title = "OH MY MOOD"
+        designContainer()
+        setEmotionTitle()
+        setLabelColor()
+        
+        for label in emotionScoreLabel {
+            label.text = "0점"
+        }
     }
 
+    func designContainer() {
+        for i in 0...containerViewCollection.count - 1 {
+            containerViewCollection[i].layer.cornerRadius = 8
+            containerViewCollection[i].backgroundColor = EmotionBgColor.allCases[i].value
+        }
+    }
+    
+    func setEmotionTitle() {
+        for i in 0...emotionTitleLabel.count - 1 {
+            emotionTitleLabel[i].text = EmotionTitle.allCases[i].rawValue
+        }
+    }
+    
+    func setLabelColor() {
+        for i in 0...emotionTitleLabel.count - 1 {
+            emotionTitleLabel[i].textColor = EmotionLabelColor.allCases[i].value
+            emotionScoreLabel[i].textColor = EmotionLabelColor.allCases[i].value
+        }
+    }
 }
